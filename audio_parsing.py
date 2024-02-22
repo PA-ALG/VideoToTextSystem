94a80508054bb7fc27e07a32cb2648c584b2a216
Sky0Wang <68536536+Sky0Wang@users.noreply.github.com>
2024-02-22 11:20:35 +0800

    Update audio_parsing.py

--- audio_parsing.py
+++ audio_parsing.py
@@ -16,10 +16,10 @@ class AudioParser:
         self.output_dir = output_dir
         self.inference_pipeline = pipeline(
             task=Tasks.auto_speech_recognition,
-            model='C:\MyCode\model\.cache\modelscope\hub\damo\speech_paraformer-large-vad-punc-spk_asr_nat-zh-cn',
+            model='/root/.cache/modelscope/hub/damo/speech_paraformer-large-vad-punc-spk_asr_nat-zh-cn',
             model_revision='v0.0.2',
-            vad_model='C:\MyCode\model\.cache\modelscope\hub\damo\speech_fsmn_vad_zh-cn-16k-common-pytorch',
-            punc_model='C:\MyCode\model\.cache\modelscope\hub\damo\punc_ct-transformer_cn-en-common-vocab471067-large',
+            vad_model='/root/.cache/modelscope/hub/damo/speech_fsmn_vad_zh-cn-16k-common-pytorch',
+            punc_model='/root/.cache/modelscope/hub/damo/punc_ct-transformer_cn-en-common-vocab471067-large',
             output_dir=output_dir,
         )
 
