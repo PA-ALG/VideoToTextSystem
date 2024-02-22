83e950368dd558f831d235bcbb8ab0b3aced412a
Sky0Wang <68536536+Sky0Wang@users.noreply.github.com>
2024-02-22 10:22:44 +0800

    Update audio_split.py

--- audio_split.py
+++ audio_split.py
@@ -17,32 +17,38 @@ class Splitter:
                     split_duration=180):  # split_duration is in seconds, 5 minutes = 300 seconds
         # 确保输出目录存在
         if not os.path.exists(output_dir):
-            os.makedirs(output_dir)
+            os.mkdir(output_dir)
 
         # 读取音频文件
         audio_clip = AudioFileClip(input_file)
         # 获取音频的总时长（秒）
         audio_duration = audio_clip.duration
-        if audio_duration > 180:
+        if audio_duration < 5:
+            os.remove(input_file)
+        elif audio_duration > 180:
             # 计算分段的数量
             num_splits = (audio_duration + split_duration - 1) // split_duration
             # 遍历每个分段并保存
             for i in range(int(num_splits)):
-                start_time = i * split_duration
-                end_time = min((i + 1) * split_duration, audio_duration)
-                # 提取分段
-                subclip = audio_clip.subclip(start_time, end_time)
-                # 构建输出文件名
-                output_file = os.path.join(output_dir,
-                                           f"{os.path.splitext(os.path.basename(input_file))[0]}_split_{i + 1}.wav")
-                # 写入分段音频
-                subclip.write_audiofile(output_file)
-                print(f"保存音频片段 {i + 1}/{num_splits}: {output_file}")
-                logger.collect(f"保存音频片段 {i + 1}/{num_splits}: {output_file}", logging.INFO)
+                try:
+                    start_time = i * split_duration
+                    end_time = min((i + 1) * split_duration, audio_duration)
+                    # 提取分段
+                    subclip = audio_clip.subclip(start_time, end_time)
+                    # 构建输出文件名
+                    output_file = os.path.join(output_dir,
+                                               f"{os.path.splitext(os.path.basename(input_file))[0]}_split_{i + 1}.wav")
+                    # 写入分段音频
+                    subclip.write_audiofile(output_file)
+                    print(f"保存音频片段 {i + 1}/{num_splits}: {output_file}")
+                    logger.collect(f"保存音频片段 {i + 1}/{num_splits}: {output_file}", logging.INFO)
+                exception Exception as e:
+                    print(e)
+                    logger.collect(f"报错原因: {e}", logging.ERROR)
             check_result = self.files_integrality_check(output_dir, num_splits)
             if check_result:
                 print(f"所有 {num_splits} 音频片段文件被保存至 {output_dir}。")
-                logger.collect(f"所有 {num_splits} 音频片段文件被保存至 {output_dir}。")
+                logger.collect(f"所有 {num_splits} 音频片段文件被保存至 {output_dir}。", logging.INFO)
                 return True
 
             else:
